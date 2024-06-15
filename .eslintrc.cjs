7ffcc6f92b56ea786f51557f2f999874dd06e7f2
pibebtol <Julius.felchow@googlemail.com>
2024-06-15 20:02:56 +0200

    add react vite base

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    'react-refresh',
+    'react',
+    '@typescript-eslint',
+    'prettier',
+  ],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    react: { version: 'detect' },
+  },
+}
