b5903fbcc5f02192bc7b23956c6dace89851ab28
pibebtol <Julius.felchow@googlemail.com>
2024-06-16 17:30:18 +0200

    some texts for context

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -5,7 +5,15 @@ export default function About() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <NavBar></NavBar>
-      Soon to come!
+      <Box component="p">
+        Hey, we are <b>russelx3</b> and <b>pibebtol</b>, two singers from
+        germany. We were searching for a tool, that specifically allows you to
+        train the high art of pitch finding.
+      </Box>
+      <Box component="p">
+        We are singing in the Kammerchor Wernigerode e.V., check us out here:
+        <a href="https://kammerchor-wernigerode.de/"> Kammerchor Wernigerode</a>
+      </Box>
     </Box>
   );
 }
--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -5,7 +5,21 @@ export default function Home() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <NavBar></NavBar>
-      Soon to come!
+      <Box component="p">
+        One of the central tasks in a choir is the finding of the pitches for
+        starting singing during a concert. If you have troubles doing that, fret
+        not, here comes the Anstimmen-Trainer. You can use it, to train your ear
+        to find the right pitches quicker and more safely. Also, it will test if
+        you are singing the right notes. Wow!
+      </Box>
+      <Box component="p">
+        The german word <b>Anstimmen</b> denotes the task of deriving the right
+        pitches for a choir from a classic 440hz tuning fork. This is a high
+        art, given the complexity of deriving the right key, as well as giving
+        the right notes in the right order for the choir. The{' '}
+        <b>Anstimmen-Trainer</b> aims to give you a tool to train this
+        profession.
+      </Box>
     </Box>
   );
 }
